473e83fb9f3481a140d4e5e77a0a1439597d77ba
DonTamime <mgn4cdgwtp@privaterelay.appleid.com>
2025-12-06 21:19:33 +0100

    Exercice 1

--- src/Person.java
+++ src/Person.java
@@ -1,27 +1,29 @@
 public class Person {
-    private  int id;
+    private int id;
     private final String name;
-    private final int surname;
-    private static int Counter = 0;
-    private Book borrowed;
-    public Person(int id, String name, int surname) {
-        this.id = Person.Counter;
+    private final String surname;
+    private final int Counter = 0;
+    private final Book borrowed;
+    public Person(String name, String surname) {
         Person.Counter++;
+        this.id = Person.Counter;
         this.name = name;
         this.surname = surname;
         this.borrowed = null;
     }
-public Book isBorrowed(boolean b) {
-        return borrowed;
+public Book getBorrowed() {
+        return this.borrowed;
 }
-        public Person(String name, int surname, Book borrowed ) {
+        public Person(String name, String surname, Book borrowed ) {
         this.name = name;
         this.surname = surname;
         this.borrowed = borrowed;
-        borrowed = isBorrowed(true);
-
-
     }
-
+public static void main (String[] args) {
+        Book book4 = new Book("Book 4", "Book 4", 40);
+        Book book5 = new Book("Book 5", "Book 5", 50);
+        Person person1 = new Person("Wick", "John", book4);
+        Person person2 = new Person("Lucas", "Marie", book5);
+}
 
 }
